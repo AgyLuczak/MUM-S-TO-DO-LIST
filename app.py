3c87a3e62c89fe2fd2782b9e173d8c31ddc14020
AgyLuczak <aaluczak1@gmail.com>
2023-09-05 12:45:59 +0000

    Create to_do_items.html and base template

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_to_do_items")
+def get_to_do_items():
+    to_do_items = mongo.db.to_do_items.find()
+    return render_template("to_do_items.html", to_do_items=to_do_items)
 
 
 if __name__ == "__main__":
