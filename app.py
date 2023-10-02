ab512854c8be661cebb1e522a103a4a0b9b4b9a9
AgyLuczak <aaluczak1@gmail.com>
2023-10-02 16:53:57 +0000

    Add admin account to display some categories to the user

--- app.py
+++ app.py
@@ -173,10 +173,16 @@ def delete_to_do_item(to_do_item_id):
     flash("Deleted!")
     return redirect(url_for("get_to_do_items"))
 
+
 @app.route("/get_categories")
 def get_categories():
-    categories = list(mongo.db.categories.find().sort("category_name", 1))
-    return render_template("categories.html", categories=categories)
+    categories = list(mongo.db.categories.find({
+        "$or": [
+            {"created_by": session["user"].lower()},
+            {"created_by": "admin"}  
+        ]
+    }).sort("category_name", 1))
+    return render_template("categories.html", categories=categories
 
 
 @app.route("/add_category", methods=["GET", "POST"])
