ca720ec2f81adea390d76c2f246b1120fc4b6067
AgyLuczak <aaluczak1@gmail.com>
2024-02-02 14:29:37 +0000

    Trigger page 500 to raise exception

--- app.py
+++ app.py
@@ -316,14 +316,22 @@ def delete_category(category_id):
 #error pages
 @app.errorhandler(404)
 def page_not_found(e):
-    
+
     return render_template('404.html'), 404
+
+# @app.errorhandler(500)
+# def internal_server_error(e):
+#     return render_template('500.html'), 500
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    print("500 error handler triggered")
     return render_template('500.html'), 500
 
 
+@app.route('/trigger-500')
+def trigger_500():
+    raise Exception("This is a test exception to trigger a 500 error.")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
